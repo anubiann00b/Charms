7d4d569dca3684a104137082e1f66707dd58b824
Shreyas <skraman1999@gmail.com>
2015-04-09 11:13:50 -0700

    Get currently playing song, doesn't work everywhere...

--- app/src/main/java/me/shreyasr/charms/MediaCharm.java
+++ app/src/main/java/me/shreyasr/charms/MediaCharm.java
@@ -1,20 +1,26 @@
 package me.shreyasr.charms;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.IBinder;
 import android.os.SystemClock;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import java.lang.reflect.Method;
 
 public class MediaCharm extends Charm {
 
-    public MediaCharm(int leftMargin, int topMargin) {
+    public MediaCharm(CharmsWindow window, int leftMargin, int topMargin) {
         super(leftMargin, topMargin);
+        window.registerReceiver(musicReciever, intentFilter);
     }
 
     @Override
@@ -65,4 +71,39 @@ public class MediaCharm extends Charm {
             e.printStackTrace();
         }
     }
+
+    static IntentFilter intentFilter;
+
+    static {
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("com.android.music.metachanged");
+        intentFilter.addAction("com.htc.music.metachanged");
+        intentFilter.addAction("fm.last.android.metachanged");
+        intentFilter.addAction("com.sec.android.app.music.metachanged");
+        intentFilter.addAction("com.nullsoft.winamp.metachanged");
+        intentFilter.addAction("com.amazon.mp3.metachanged");
+        intentFilter.addAction("com.miui.player.metachanged");
+        intentFilter.addAction("com.real.IMP.metachanged");
+        intentFilter.addAction("com.sonyericsson.music.metachanged");
+        intentFilter.addAction("com.rdio.android.metachanged");
+        intentFilter.addAction("com.samsung.sec.android.MusicPlayer.metachanged");
+        intentFilter.addAction("com.andrew.apollo.metachanged");
+        intentFilter.addAction("com.spotify.music.metadatachanged");
+    }
+
+    static BroadcastReceiver musicReciever = new BroadcastReceiver() {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d("Received", intent.getAction());
+            String action = intent.getAction();
+            String cmd = intent.getStringExtra("command");
+            Log.v("tag ", action + " / " + cmd);
+            String artist = intent.getStringExtra("artist");
+            String album = intent.getStringExtra("album");
+            String track = intent.getStringExtra("track");
+            Log.v("tag", artist + ":" + album + ":" + track);
+            Toast.makeText(ApplicationWrapper.getInstance(), track, Toast.LENGTH_SHORT).show();
+        }
+    };
 }
